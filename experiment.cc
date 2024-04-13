d93cb529e68a2024908223eaeeb41e0245039ff2
Sergey Bars <ssbars137@gmail.com>
2024-04-13 11:17:11 +0300

    unique_ptr vec example

--- experiment.cc
+++ experiment.cc
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+#include <memory>
 
 class Calculation {
     public:
+    // This version doesn't work
+    std::vector<double> squares(double x, int n) {
+        std::vector<double> sq{};
+        for (int i = 1; i <= n; ++i) {
+            sq.push_back(std::pow(x,i));
+        }
+        return sq;
+    }
+
+    std::unique_ptr<std::vector<double>> squares_unique_ptr(double x, int n) {
+        std::unique_ptr<std::vector<double>> sq{};
+        for (int i = 1; i <= n; ++i) {
+            sq->push_back(std::pow(x,i));
+        }
+        return sq;
+    }
+
     double operator()(double x) {
-        return x * x;
+        double sm = 0;
+        //auto v = squares(x, 3); // Not working version
+        auto v = squares_unique_ptr(x, 3);
+        for (auto& x: *v) {
+            sm += x;
+        }
+        return sm;
     }
 };
 
@@ -22,6 +50,6 @@ double foo(Calculation& calc, double x) {return calc(x);}
 int main() {
     Calculation calc{};
     double x =  2.0;
-    std::cout << "x = " << x << "x^2 = " << calc(x) << " (x^2)' = " << __enzyme_autodiff<double>(&foo, enzyme_const, &calc, enzyme_out, x);
+    std::cout << "x = " << x << "f(x) = " << calc(x) << " f'(x)' = " << __enzyme_autodiff<double>(&foo, enzyme_const, &calc, enzyme_out, x);
     return 0;
 }
